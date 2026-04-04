50211e1107ca958a5b1af22940d0ef05bd5b1ffd
Archipall <64artem.robota7@gmail.com>
2026-04-04 17:07:56 +0300

    Pets Shelter 2/8-case

--- Monster_Vs_hero_001.cs
+++ Monster_Vs_hero_001.cs
@@ -1,25 +1,25 @@
-﻿Random attak = new Random();
+﻿// Random attak = new Random();
 
-int hero_health = 10;
-int monster_health = 10;
+// int hero_health = 10;
+// int monster_health = 10;
 
-//int monster_attak = attak.Next(1,11);
-//int hero_attak= attak.Next(1,10);
+// //int monster_attak = attak.Next(1,11);
+// //int hero_attak= attak.Next(1,10);
 
-while(hero_health > 0 && monster_health > 0)
-{
-    int monster_attak = attak.Next(1,11);
-    int hero_attak= attak.Next(1,10);
+// while(hero_health > 0 && monster_health > 0)
+// {
+//     int monster_attak = attak.Next(1,11);
+//     int hero_attak= attak.Next(1,10);
 
-    Console.WriteLine($"Hero attaked monster {hero_attak} dmg  monster have {monster_health - hero_attak}");
-    monster_health = monster_health - hero_attak;
+//     Console.WriteLine($"Hero attaked monster {hero_attak} dmg  monster have {monster_health - hero_attak}");
+//     monster_health = monster_health - hero_attak;
     
-    if(monster_health <= 0) break;
+//     if(monster_health <= 0) break;
 
-    Console.WriteLine($"Monster attak {monster_attak} and hero have {hero_health - monster_attak}");
-    hero_health = hero_health - monster_attak;
+//     Console.WriteLine($"Monster attak {monster_attak} and hero have {hero_health - monster_attak}");
+//     hero_health = hero_health - monster_attak;
 
 
-}
+// }
 
-Console.WriteLine(hero_health > monster_health ? "Hero wins!" : "Monster wins!");
+// Console.WriteLine(hero_health > monster_health ? "Hero wins!" : "Monster wins!");
